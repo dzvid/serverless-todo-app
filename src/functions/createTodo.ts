c304bf62b7b9a11e4623bb6908017001f0491ce2
David <oliveiradavid.dev@gmail.com>
2021-08-30 15:56:36 -0400

    feat: Create todo

---
+++ src/functions/createTodo.ts
@@ -0,0 +1,46 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { v4 as uuidV4 } from "uuid";
+import { document } from "../utils/dynamodbClient";
+
+interface ICreateTodo {
+  title: string;
+  deadline: string;
+}
+
+interface ITodo {
+  id: string;
+  user_id: string;
+  title: string;
+  done: boolean;
+  deadline: Date;
+}
+
+export const handle: APIGatewayProxyHandler = async (event) => {
+  const { user_id } = event.pathParameters;
+  const { title, deadline } = JSON.parse(event.body) as ICreateTodo;
+
+  const todo: ITodo = {
+    id: uuidV4(),
+    user_id,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+  };
+
+  await document
+    .put({
+      TableName: "users_todos",
+      Item: todo,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Created!",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
